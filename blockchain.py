3f10ee394961dbc85f928698e9bdfd488fd418bb
nicholaas2cheung <nicholascheung25.gtd@gmail.com>
2021-07-24 13:09:38 +0800

    set up test environemt for crypto

---
+++ blockchain.py
@@ -0,0 +1,26 @@
+from block import Block
+
+class Blockchain:
+	"""
+	Blockchain: a public ledger of transactions.
+	Implemented as a l;ist of blocks - data sets of transactions
+	"""
+	def __init__(self):
+		self.chain = [Block.genesis()]
+
+	def __repr__(self):
+		return f'Blockchain: {self.chain}'
+
+	def add_block(self, data):
+		self.chain.append(Block.mine_block(self.chain[-1], data))
+
+def main():
+	blockchain = Blockchain()
+	blockchain.add_block('One')
+	blockchain.add_block('Two')
+
+	print(blockchain)
+	print(f'blockchain.py __name__:{__name__}')
+
+if __name__ == '__main__':
+	main()
