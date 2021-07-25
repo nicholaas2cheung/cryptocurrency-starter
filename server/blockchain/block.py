8c6c3380d1ccba67167d4a95443350e12372d5f5
nicholaas2cheung <nicholascheung25.gtd@gmail.com>
2021-07-25 20:56:52 +0800

    added chain validation and replacement

--- server/blockchain/block.py
+++ server/blockchain/block.py
@@ -36,6 +36,9 @@ class Block:
 			f'nonce: {self.nonce})'
 		)
 
+	def __eq__(self, other):
+		return self.__dict__ == other.__dict__
+
 	@staticmethod
 	def mine_block(last_block, data):
 		"""
@@ -92,10 +95,34 @@ class Block:
 		if block.last_hash != last_block.hash:
 			raise Exception('The block last_hash must be correct')
 
+		if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+			raise Exception('The proof of work requirement was not met')
+
+		if abs(last_block.difficulty - block.difficulty) > 1:
+			raise Exception('The block difficulty must only adjust by 1')
+
+		reconstructed_hash = crypto_hash(
+			block.timestamp,
+			block.last_hash,
+			block.data,
+			block.difficulty,
+			block.nonce
+		)
+
+		if block.hash != reconstructed_hash:
+			raise Exception('The block hash must be correct')
+
 def main():
 	genesis_block = Block.genesis()
-	block = Block.mine_block(genesis_block, 'foo')
-	print(block)
+	bad_block = Block.mine_block(genesis_block, 'foo')
+	bad_block.last_hash = 'evil_data'
+
+	try:
+		Block.is_valid_block(genesis_block, bad_block)
+	except Exception as e:
+		print(f'is valid block: {e}')
+
+	Block.is_valid_block(genesis_block, bad_block)
 
 """
 if __name == '__main__', it means we are executing the currency python script.
